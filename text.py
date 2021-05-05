d43112f587837a02581d7c4581088e2782449747
epicureanheron <joethor1989@gmail.com>
2021-05-05 05:54:58 -0500

    updated with duplicate value checking

--- text.py
+++ text.py
@@ -8,9 +8,15 @@ def get_unique_words(text):
     split_text = lower_text.split(' ')
     # sorts values in list 
     split_text.sort()
-    
-    print(split_text)
-
+    # empty list to populate unique words
+    results_list = []
+    # iterate over the list
+    for word in split_text:
+        # check to see if value is already in results lists
+        if word not in results_list:
+            # append the word if it is unique
+            results_list.append(word)
+    print(results_list)
 text = "The quick brown fox jumps over the lazy dog!"
 
 get_unique_words(text)
